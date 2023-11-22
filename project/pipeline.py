59b80045f668561a0bb8445a1e1e8eb66d1349d2
rachanakafle <rachanakafle32@gmail.com>
2023-11-22 16:04:00 +0100

    pull the dataset

---
+++ project/pipeline.py
@@ -0,0 +1,31 @@
+import opendatasets as od
+import pandas as pd
+#download covid-19 asian countries dataset
+od.download(
+	"https://www.kaggle.com/datasets/vivek468/asia-covid-19-cases-updated-10-oct-21/data")
+
+# download covid-19 european countries dataset
+od.download(
+	"https://www.kaggle.com/datasets/anandhuh/latest-covid19-data-of-european-countries")
+
+
+# Read the asia covid data
+file_path = 'asia-covid-19-cases-updated-10-oct-21/AsiaCases_.csv'
+df_asia_covid = pd.read_csv(file_path)
+print(df_asia_covid.head())
+
+# Read the europe covid data
+file_path = 'latest-covid19-data-of-european-countries/europe_covid.csv'
+df_europe_covid = pd.read_csv(file_path)
+print(df_europe_covid.head())
+
+# select only relevent columns
+'''
+Country
+Total Cases
+Total Deaths
+Total Recovered
+Active Cases
+Total Tests
+Population
+'''
