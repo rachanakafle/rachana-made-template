7cf4dfc32bbc16f9730f796cd2d2098dc03e9a5a
rachanakafle <rachanakafle32@gmail.com>
2023-11-29 13:11:41 +0100

    write data into a SQLite database

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -13,5 +13,38 @@ print(df.head())
 # Requirement 1: Drop the "Status" Column
 df = df.drop(column=['Status'])
 
+# Requirement 2: validation
+df['Laenge'] = df['Laenge'].str.replace(r',', '.')
+df['Breite'] = df['Breite'].str.replace(r',', '.')
+df = df[df['Verkehr'].isin(['FV', 'nur DPN', 'RV'])]
+df = df[ (df['Laenge'] <= 90) & (df['Laenge'] >= -90)]
+df = df[ (df['Breite'] <= 90) & (df['Breite'] >= -90)]
+pattern = re.compile(r"^\w{2}:\d+:\d+(?::\d+)?$")
+valid_ifopt = df["IFOPT"].astype(str).str.contains(pattern, regex=True)
+df = df.loc[valid_ifopt]
+#drop rows with empty cells
+df = df.dropna()
+
+# Define columns datatypes
+columns_types = {
+    "DS100": str,
+    "EVA_NR": int,
+    "Laenge": float,
+    "Breite": float,
+    "Verkehr": str,
+    "IFOPT": str,
+    "NAME": str,
+    "Betreiber_Name": str,
+    "Betreiber_Nr": int
+
+}
+# Step 3: Data Loading
+table_names = "trainstops"
+connection=create_engine("sqlite:///trainstops.sqlite", echo=True)
+
+# Write the DataFrame to SQLite database
+df.to_sql(table_names, connection, if_exists='replace',index=False,dtype=columns_types)
+
+
 
 
