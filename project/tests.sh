ec9f1293a7b60e76833c11ac1e98e7afe4e49473
rachanakafle <rachanakafle32@gmail.com>
2023-12-07 10:10:20 +0100

    aded tests.sh file

---
+++ project/tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# script to exit immediately if any command exits with a non-zero status (indicating an error).
+set -e
+
+# Export kaggle.json to os env for Kaggle authentication
+KAGGLE_JSON_PATH="./project/kaggle.json"
+KAGGLE_CONFIG_DIR=$(dirname "$KAGGLE_JSON_PATH")
+export KAGGLE_CONFIG_DIR
+
+# Run testcase
+pytest ./project/tests/test_pipeline.py
