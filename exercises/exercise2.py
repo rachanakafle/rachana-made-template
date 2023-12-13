a906410608f9aaa5be476e3954bd5bd8df55ebb8
rachanakafle <rachanakafle32@gmail.com>
2023-12-13 14:40:29 +0100

    exercise 2 error fixed

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -11,11 +11,11 @@ df = pd.read_csv(data_url,delimiter=';')
 print(df.head())
 
 # Requirement 1: Drop the "Status" Column
-df = df.drop(column=['Status'])
+df = df.drop(columns=['Status'])
 
 # Requirement 2: validation
-df['Laenge'] = df['Laenge'].str.replace(r',', '.')
-df['Breite'] = df['Breite'].str.replace(r',', '.')
+df['Laenge'] = df['Laenge'].str.replace(r',', '.').astype(float)
+df['Breite'] = df['Breite'].str.replace(r',', '.').astype(float)
 df = df[df['Verkehr'].isin(['FV', 'nur DPN', 'RV'])]
 df = df[ (df['Laenge'] <= 90) & (df['Laenge'] >= -90)]
 df = df[ (df['Breite'] <= 90) & (df['Breite'] >= -90)]
@@ -27,15 +27,15 @@ df = df.dropna()
 
 # Define columns datatypes
 columns_types = {
-    "DS100": str,
-    "EVA_NR": int,
-    "Laenge": float,
-    "Breite": float,
-    "Verkehr": str,
-    "IFOPT": str,
-    "NAME": str,
-    "Betreiber_Name": str,
-    "Betreiber_Nr": int
+    "DS100": TEXT,
+    "EVA_NR": Integer,
+    "Laenge": FLOAT,
+    "Breite": FLOAT,
+    "Verkehr": Text,
+    "IFOPT": Text,
+    "NAME": Text,
+    "Betreiber_Name": Text,
+    "Betreiber_Nr": Integer
 
 }
 # Step 3: Data Loading
