319cd451f9691a63599fbb55cb452a2ab7aef156
rachanakafle <rachanakafle32@gmail.com>
2023-12-20 15:01:51 +0100

    install required packages automatically

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -2,4 +2,10 @@ KAGGLE_JSON_PATH="./project/kaggle.json"
 KAGGLE_CONFIG_DIR=$(dirname "$KAGGLE_JSON_PATH")
 export KAGGLE_CONFIG_DIR
 
+# Install required packages
+pip install --upgrade pip
+pip install -r ./project/requirements.txt
+
 python3 ./project/pipeline.py
+
+echo "Pipeline completed"
--- project/tests.sh
+++ project/tests.sh
@@ -7,5 +7,9 @@ KAGGLE_JSON_PATH="./project/kaggle.json"
 KAGGLE_CONFIG_DIR=$(dirname "$KAGGLE_JSON_PATH")
 export KAGGLE_CONFIG_DIR
 
+#Install required packages
+pip install --upgrade pip
+pip install -r ./project/requirements.txt
+
 # Run testcase
 pytest ./project/tests/test_pipeline.py
