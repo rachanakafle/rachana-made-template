ddc7004b720835e3b0fd79c298544490e1efcb07
rachanakafle <rachanakafle32@gmail.com>
2024-01-24 01:10:58 +0100

    select and rename columns

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -1,6 +1,6 @@
 import urllib.request
 import zipfile
-
+import pandas as pd
 
 # Download and unzip data
 download_link = "https://www.mowesta.com/data/measure/mowesta-dataset-20221107.zip"
@@ -8,3 +8,28 @@ path = "mowesta-dataset-20221107.zip"
 urllib.request.urlretrieve(download_link, path)
 with zipfile.ZipFile(path, 'r') as zip_ref:
     zip_ref.extractall('mowesta-dataset-20221107')
+
+#reading the data and selecting specific columns
+df = pd.read_csv(
+    "./mowesta-dataset-20221107/data.csv",
+    delimiter=";",
+    decimal=",",
+    index_col=False,
+    usecols=[
+        "Geraet",
+        "Hersteller",
+        "Model",
+        "Monat",
+        "Temperatur in °C (DWD)",
+        "Batterietemperatur in °C",
+        "Geraet aktiv",
+    ],
+)  
+# renaming columns
+df = df.rename(
+    columns={
+        "Temperatur in °C (DWD)": "Temperatur",
+        "Batterietemperatur in °C": "Batterietemperatur",
+    }
+)
+##
